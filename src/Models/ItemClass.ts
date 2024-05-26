da86bade9a083bac990641fcb13c0d87d8c67b88
AdamopoulosRKK <gkadamopoulos@gmail.com>
2024-05-26 17:28:03 +0300

    use dummy id instead of crypto

--- src/Models/ItemClass.ts
+++ src/Models/ItemClass.ts
@@ -5,10 +5,23 @@ export class ItemClass {
   constructor(text: string) {
     this.text = text;
     this.done = false;
-    this.id = crypto.randomUUID();
+    this.id = makeid(10);
   }
 
   toggleDone() {
     this.done = !this.done;
   }
 }
+
+const makeid = (length: number): string => {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+};
